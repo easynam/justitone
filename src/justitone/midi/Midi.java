a52033f985d00194d0c2df659a319e7eb066ce3f
easynam <easynam@gmail.com>
2019-04-12 23:28:31 +0100

    midi tempo

--- src/justitone/midi/Midi.java
+++ src/justitone/midi/Midi.java
@@ -1,6 +1,7 @@
 package justitone.midi;
 
 import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MetaMessage;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.ShortMessage;
@@ -13,6 +14,11 @@ import justitone.jidi.JidiTrack;
 public class Midi {
     public Sequence jidiToMidi(JidiSequence jidi, float pitchBendRange) throws InvalidMidiDataException {
         Sequence seq = new Sequence(Sequence.PPQ, jidi.ppm/4);
+
+        Track tempoTrack = seq.createTrack();
+        MetaMessage message = new MetaMessage();
+        message.setMessage(0x51, encodeTempo(jidi.bpm), 3);
+        tempoTrack.add(new MidiEvent(message, 0));
         
         int channel = 0;
         
@@ -45,8 +51,20 @@ public class Midi {
             
         byte[] encoded = new byte[2];
             
-        encoded[0] = (byte) (translated & 0x7F);           // Least significant "byte"
-        encoded[1] = (byte) ((translated & 0x3F80) >> 7);  // Most significant "byte"
+        encoded[0] = (byte) (translated & 0x7F);
+        encoded[1] = (byte) ((translated & 0x3F80) >> 7);
+            
+        return encoded;
+    }
+    
+    public byte[] encodeTempo(int bpm) {
+        int micros = (int) 60000000f/bpm;
+        
+        byte[] encoded = new byte[3];
+            
+        encoded[2] = (byte) (micros & 0xFF);
+        encoded[1] = (byte) ((micros & 0xFF00) >> 8);
+        encoded[0] = (byte) ((micros & 0xFF0000) >> 16);
             
         return encoded;
     }
@@ -84,23 +102,23 @@ public class Midi {
     private void writeNoteOn(Track track, float freq, long tick, float pitchBendRange, int channel) throws InvalidMidiDataException {
         float note = note(freq);
         
-        ShortMessage mm = new ShortMessage();
-        mm.setMessage(ShortMessage.NOTE_ON | channel, midiNote(note) & 0xff, 0x60);
-        track.add(new MidiEvent(mm, tick));
+        ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.NOTE_ON | channel, midiNote(note) & 0xff, 0x60);
+        track.add(new MidiEvent(message, tick));
         
         byte[] pitchBend = midiPitchBend(note, pitchBendRange);
         
-        mm = new ShortMessage();
-        mm.setMessage(ShortMessage.PITCH_BEND | channel, pitchBend[0], pitchBend[1]);
-        track.add(new MidiEvent(mm, tick));
+        message = new ShortMessage();
+        message.setMessage(ShortMessage.PITCH_BEND | channel, pitchBend[0], pitchBend[1]);
+        track.add(new MidiEvent(message, tick));
     }
     
     private void writeNoteOff(Track track, float freq, long tick, int channel) throws InvalidMidiDataException {
         float note = note(freq);
         
-        ShortMessage mm = new ShortMessage();
-        mm.setMessage(ShortMessage.NOTE_OFF | channel, midiNote(note), 0x40);
-        track.add(new MidiEvent(mm, tick));
+        ShortMessage message = new ShortMessage();
+        message.setMessage(ShortMessage.NOTE_OFF | channel, midiNote(note), 0x40);
+        track.add(new MidiEvent(message, tick));
     }
     
 }
