2d19f53a20b353df439779de35e319a68b728e80
easynam <easynam@gmail.com>
2019-04-15 19:37:49 +0100

    CLI for discord bot

---
+++ src/justitone/cli/CLI.java
@@ -0,0 +1,50 @@
+package justitone.cli;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+
+import justitone.Song;
+import justitone.jidi.JidiSequence;
+import justitone.midi.Midi;
+import justitone.parser.Reader;
+
+public class CLI {
+    public static void main(String args[]) throws InvalidMidiDataException, IOException {
+        if (args.length < 1) System.exit(0);
+        
+        String fileName = args[0];
+        
+        Scanner in = new Scanner(System.in);
+        
+        StringBuilder s = new StringBuilder();
+        
+        while (in.hasNext()) {
+            s.append(in.nextLine());
+            s.append("\n");
+        }
+
+        String source = s.toString();
+        
+        in.close();
+
+        Reader reader = new Reader();
+        
+        Song song = reader.parse(source);
+        
+        if (song.sequence.events.isEmpty()) System.exit(1);
+            
+        JidiSequence seq = new JidiSequence(song, 768);
+            
+        Midi midi = new Midi();
+            
+        Sequence midiSeq = midi.jidiToMidi(seq, 2f);
+            
+        File f = new File(fileName);
+        MidiSystem.write(midiSeq, 1, f);
+    }
+}
