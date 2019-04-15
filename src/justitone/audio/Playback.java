6fc3d300e5e306cd0d904a9f41c4da081b0d0b69
easynam <easynam@gmail.com>
2019-04-15 18:40:44 +0100

    minor cleanup

--- src/justitone/audio/Playback.java
+++ src/justitone/audio/Playback.java
@@ -1,13 +1,8 @@
 package justitone.audio;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 
-import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
@@ -35,7 +30,7 @@ public class Playback implements Runnable {
     }
 
     public static ConcurrentLinkedQueue<Message> start() {
-        ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue();
+        ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<>();
         (new Thread(new Playback(queue))).start();
         return queue;
     }
@@ -72,7 +67,7 @@ public class Playback implements Runnable {
 
             // fill buffer with samples
             if (playing) {
-                float length = (float)sequence.ppm * ((float)sequence.bpm / 60) * ((float)bufSize / (float)sampleRate) / 4;
+                float length = sequence.ppm * (sequence.bpm / 60f) * ((float) bufSize / sampleRate) / 4f;
 
                 if (sequence != null) {
                     for (JidiTrack track : sequence.tracks) {
@@ -80,18 +75,18 @@ public class Playback implements Runnable {
                         for (JidiEvent event : track.events) {
                             if (event instanceof JidiEvent.NoteOn) {
                                 JidiEvent.NoteOn note = (JidiEvent.NoteOn)event;
-                                float start = ticksToSeconds((float)(note.tick - tick) - offset, sequence.bpm, sequence.ppm);
+                                float start = ticksToSeconds(note.tick - tick - offset, sequence.bpm, sequence.ppm);
                                 float end;
                                 if (e + 1 < track.events.size()) {
-                                    end = ticksToSeconds((float)(track.events.get(e + 1).tick - tick) - offset, sequence.bpm, sequence.ppm);
+                                    end = ticksToSeconds(track.events.get(e + 1).tick - tick - offset, sequence.bpm, sequence.ppm);
                                 } else {
                                     end = ticksToSeconds(length, sequence.bpm, sequence.ppm);
                                 }
-                                int startSample = Math.max(0, (int)Math.ceil(start * sampleRate));
-                                int endSample = Math.min(bufSize, (int)Math.floor(end * sampleRate) + 1);
+                                int startSample = Math.max(0, (int) Math.ceil(start * sampleRate));
+                                int endSample = Math.min(bufSize, (int) Math.floor(end * sampleRate) + 1);
                                 for (int i = startSample; i < endSample; i++) {
                                     short current = buf.getShort(i * 2);
-                                    float phase = 2 * (float)Math.PI * note.freq * ((float)i / (float)sampleRate - start);
+                                    float phase = 2 * (float)Math.PI * note.freq * ((float)i / sampleRate - start);
                                     short value = (short)(current + Short.MAX_VALUE * 0.1 * Math.sin(phase));
                                     buf.putShort(i * 2, value);
                                 }
@@ -115,8 +110,8 @@ public class Playback implements Runnable {
         line.close();
     }
 
-    static float ticksToSeconds(float tick, int bpm, int ppm) {
-        return 4 * 60 * ((tick / (float)ppm) / (float)bpm);
+    static float ticksToSeconds(float tick, float bpm, float ppm) {
+        return 4 * 60 * (tick / ppm) / bpm;
     }
 
     // public static void save(Track channel) throws IOException {
